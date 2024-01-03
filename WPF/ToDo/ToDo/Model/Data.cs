f67ea4ddb41cde5c638ae6d1574c14f784138080
Afgan <134388584+xxefi@users.noreply.github.com>
2024-01-03 19:48:52 +0400

    Add files via upload

---
+++ WPF/ToDo/ToDo/Model/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Model
+{
+    public class Data
+    {
+        public string ToDoListName {  get; set; }
+        public DateTime TimeCreate { get; set; }
+    }
+}
