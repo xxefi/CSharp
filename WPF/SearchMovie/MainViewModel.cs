6e76a187d35c12f7de0c67f0d771ff55238c3df6
Afgan <134388584+xxefi@users.noreply.github.com>
2024-01-03 20:14:29 +0400

    Add files via upload

---
+++ WPF/SearchMovie/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace SearchMovie;
+
+public class MainViewModel : ViewModelBase
+{
+    private string _search;
+    private ObservableCollection<MovieModel> _viewModel;
+    private readonly MovieSearchService _searchService;
+
+
+    public string SearchText
+    {
+        get { return _search; }
+        set
+        {
+            Set(ref _search, value);
+        }
+
+    }
+    
+    public ObservableCollection<MovieModel> SearchResults
+    {
+        get { return _viewModel; }
+        set
+        {
+            Set(ref _viewModel, value);
+        }
+    }
+
+    public ICommand SearchCommand { get; }
+    
+
+    public MainViewModel()
+    {
+        _searchService = new MovieSearchService();
+        SearchCommand = new RelayCommand(Search);
+
+    }
+
+    private void Search()
+    {
+        if (string.IsNullOrEmpty(_search))
+        {
+            return;
+        }
+        var results = _searchService.SearchMovies(SearchText);
+        
+        SearchResults = new ObservableCollection<MovieModel>(results);
+
+        MessageBox.Show("Search command executed!");
+    }
+}
---
+++ WPF/SearchMovie/MovieModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchMovie;
+
+public class MovieModel
+{
+    public string Title { get; set; }
+    public int Year { get; set; }
+    public string Id { get; set; }  
+}
