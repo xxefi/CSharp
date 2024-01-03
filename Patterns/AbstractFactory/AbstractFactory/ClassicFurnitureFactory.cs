ef161d0d4e1be1b9e99ee16a4edf1ce28bde5f72
Afgan <134388584+xxefi@users.noreply.github.com>
2024-01-03 19:38:15 +0400

    Add files via upload

---
+++ Patterns/AbstractFactory/AbstractFactory/ClassicFurnitureFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory;
+
+public class ClassicFurnitureFactory : IFurnitureFactory
+{
+    public ITable CreateTable()
+    {
+        return new WoodenTable();
+    }
+
+    public IChair CreateChair()
+    {
+        return new WoodenChair();
+    }
+}
---
+++ Patterns/AbstractFactory/AbstractFactory/GlassTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory;
+
+public class GlassTable : ITable
+{
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Тип: Стеклянный стол");
+        Console.WriteLine("Материал: Стекло");
+        Console.WriteLine("Стиль: Современный");
+        Console.WriteLine();
+    }
+}
