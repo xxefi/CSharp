db7fb2d8d69914c032fceb536b229e26b7ea0e72
Afgan <134388584+xxefi@users.noreply.github.com>
2024-03-11 22:36:53 +0400

    Add files via upload

--- WPF/ToDo/ToDo/App.xaml.cs
+++ WPF/ToDo/ToDo/App.xaml.cs
@@ -1,6 +1,14 @@
-﻿using System.Configuration;
+﻿using GalaSoft.MvvmLight.Messaging;
+using GalaSoft.MvvmLight.Views;
+using SimpleInjector;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Controls;
+using ToDo.Interfaces;
+using ToDo.Services;
+using ToDo.ViewModel;
+using ToDo.Views;
 
 namespace ToDo
 {
@@ -9,6 +17,28 @@ namespace ToDo
     /// </summary>
     public partial class App : Application
     {
+        public static Container Container { get; set; }
+        void Register()
+        {
+            Container = new Container();
+            Container.RegisterSingleton<IMessenger, Messenger>();
+            Container.RegisterSingleton<Interfaces.INavigationService, NavigationService>();
+            Container.RegisterSingleton<ToDoDbContext>();
+            Container.RegisterSingleton<MainViewModel>();
+            Container.RegisterSingleton<AddToDoViewModel>(); 
+
+
+            Container.Register<ToDoViewModel>();
+            Container.Register<RemoveToDoViewModel>();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Register();
+            var window = new MainView();
+            window.DataContext = Container.GetInstance<MainViewModel>();
+            window.Show();
+        }
     }
 
 }
---
+++ WPF/ToDo/ToDo/ToDoDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using ToDo.Models;
+
+namespace ToDo
+{
+    public class ToDoDbContext : DbContext
+    {
+
+
+        public ToDoDbContext()
+        {
+
+        }
+
+        public DbSet<ToDoModel> ToDo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                optionsBuilder.UseSqlServer(builder.GetConnectionString("Default"));
+            }
+        }
+
+    }
+}
